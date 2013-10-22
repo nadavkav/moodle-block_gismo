f1c52c440b840f79ca3a1a07403321cc7d8968c0
Christian Milani <milanic@usi.ch>
2013-10-22 10:31:46 +0200

    Updated version 2013101501 & fixed french translations problems

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
-    $plugin->version = 2013101500;  //YYYYMMDDHH (year, month, day, 24-hr time)
+    $plugin->version = 2013101501;  //YYYYMMDDHH (year, month, day, 24-hr time)
     $plugin->requires = 2012062500; //Moodle 2.4 2012120300 - Moodle 2.3 2012062500  (This is the release version for Moodle 2.3)
     $plugin->component = 'block_gismo'; // Full name of the plugin (used for diagnostics)
     $plugin->cron = 900;
