ad9e75b3fc4b0e10f4d40acbb4528f0ae5351d1d
steve-o <milanic@usi.ch>
2014-10-28 16:40:27 +0100

    Updated version 3.3 and requires moodle 2.7.x

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
-    $plugin->version = 2013101501;  //YYYYMMDDHH (year, month, day, 24-hr time)
-    $plugin->requires = 2012062500; //Moodle 2.4 2012120300 - Moodle 2.3 2012062500  (This is the release version for Moodle 2.3)
+    $plugin->version = 2014102800;  //YYYYMMDDHH (year, month, day, 24-hr time)
+    $plugin->requires = 2014051200; //Moodle 2.7
     $plugin->component = 'block_gismo'; // Full name of the plugin (used for diagnostics)
     $plugin->cron = 900;
     $plugin->maturity = MATURITY_STABLE; 
-    $plugin->release = '3.2 (Build: 2013101500)';
+    $plugin->release = '3.3 (Build: 2014102800)';
