d1b709cdd89e6a7fdea93e2e47b3950173e9b875
steve-o <steve-o@steveo-HP-EliteOne-800-G1-AiO>
2014-12-04 11:16:45 +0100

    Formatted code and updated release date

--- version.php
+++ version.php
@@ -1,7 +1,14 @@
 <?php
-    $plugin->version = 2014102800;  //YYYYMMDDHH (year, month, day, 24-hr time)
-    $plugin->requires = 2014051200; //Moodle 2.7
-    $plugin->component = 'block_gismo'; // Full name of the plugin (used for diagnostics)
-    $plugin->cron = 900;
-    $plugin->maturity = MATURITY_STABLE; 
-    $plugin->release = '3.3 (Build: 2014102800)';
+
+/**
+ * GISMO block
+ *
+ * @package    block_gismo
+ * @copyright  eLab Christian Milani
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$plugin->version = 2014120300;  //YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2014051200; //Moodle 2.7
+$plugin->component = 'block_gismo'; // Full name of the plugin (used for diagnostics)
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '3.3 (Build: 2014120300)';
