3644956fe45f5a129127e4bf1af46d4f1a6bee56
steve-o <steve-o@steveo-HP-EliteOne-800-G1-AiO>
2014-12-04 11:23:36 +0100

    Formatted code
    modified require_once config.php as in the other files

--- lib/gismo/server_side/reset_data.php
+++ lib/gismo/server_side/reset_data.php
@@ -1,13 +1,21 @@
 <?php
-    require_once "../../../../../config.php";
-        
-    // trace start
-    echo "GISMO - reset data (start)!<br />";
-    
-    $gdm = new block_gismo\GISMOdata_manager(true);
-    
-    $gdm->devel_mode_reset();
-    
-    echo "GISMO - reset data (end)!<br />";
-    
+
+/**
+ * GISMO block
+ *
+ * @package    block_gismo
+ * @copyright  eLab Christian Milani
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+define('ROOT', (realpath(dirname( __FILE__ )) . DIRECTORY_SEPARATOR));
+require_once realpath(ROOT . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "config.php");
+
+// trace start
+echo "GISMO - reset data (start)!<br />";
+
+$gdm = new block_gismo\GISMOdata_manager(true);
+
+$gdm->devel_mode_reset();
+
+echo "GISMO - reset data (end)!<br />";
 ?>
