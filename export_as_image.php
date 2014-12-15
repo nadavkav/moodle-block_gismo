a8eb7df97f260824d16e0b9a630f8acfafb50869
steve-o <steve-o@steveo-HP-EliteOne-800-G1-AiO>
2014-12-15 17:09:04 +0100

    Changed all $_REQUEST to optional_param

--- export_as_image.php
+++ export_as_image.php
@@ -7,12 +7,20 @@
  * @copyright  eLab Christian Milani
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+// include moodle config file
+define('ROOT', substr(realpath(dirname(__FILE__)), 0, stripos(realpath(dirname(__FILE__)), "blocks", 0)) . 'blocks/gismo/');
+require_once realpath(ROOT . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "config.php");
+
+$data = required_param('data',PARAM_RAW);
+$chart_id_val = required_param('chart_id',PARAM_RAW);
 // data
 $img_mime = "image/png";
-$img_data = (isset($_POST["data"])) ? base64_decode($_POST["data"]) : "";
+$img_data = (isset($data)) ? base64_decode($data) : "";
 $chart_id = "";
-if (isset($_POST["chart_id"])) {
-    $pieces = explode("-", $_POST["chart_id"]);
+
+if (isset($chart_id_val)) {
+    $pieces = explode("-", $chart_id_val);
     if (is_array($pieces) AND count($pieces) > 0) {
         foreach ($pieces as $p) {
             $chart_id .= ucfirst(str_replace(array(" ", ".", "_", "\\", "/"), array(""), trim($p)));
