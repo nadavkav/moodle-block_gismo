84af7da73ed4e03b39212eac04088df38f53bbcd
steve-o <steve-o@steveo-HP-EliteOne-800-G1-AiO>
2014-12-16 11:55:34 +0100

    Thanks to Tim Lock: Solved postgre query problem

--- ajax.php
+++ ajax.php
@@ -21,7 +21,7 @@ $q = optional_param('q', '', PARAM_TEXT);
 $from = optional_param('from', '', PARAM_INT);
 $to = optional_param('to', '', PARAM_INT);
 $subtype = optional_param('subtype', '', PARAM_TEXT);
-$id =  optional_param('id', '', PARAM_INT);
+$id = optional_param('id', '', PARAM_INT);
 
 // include specific libraries for quizzes
 // require_once realpath(ROOT . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "mod"  . DIRECTORY_SEPARATOR . "quiz" . DIRECTORY_SEPARATOR . "lib.php");
@@ -116,13 +116,14 @@ switch ($query) {
         $result->name = get_string($lang_index, "block_gismo");
         // links
         $result->links = null;
-        
+
         $student_resource_access = false;
-        //postgreSQL solve problem on GROUP BY
-        if($CFG->dbtype === "pgsql"){           
-            $student_resource_access = $DB->get_records_sql("SELECT ROW_NUMBER() over(), a.* FROM (SELECT course, userid, timedate, sum(numval) as numval FROM {block_gismo_sl} WHERE course = ? AND time BETWEEN ? AND ? AND userid IN (?,?) GROUP BY course, timedate, userid ORDER BY timedate ASC) as a",$ctu_params);
-        }else{
-            $ctu_filters .= " GROUP BY course, timedate, userid"; //BUG FIX WHEN GISMO EXPORTER RUN MORE THEN ONCE A DAY, we need to group by course,timedate & USERID
+        $ctu_filters .= " GROUP BY course, timedate, userid"; //BUG FIX WHEN GISMO EXPORTER RUN MORE THEN ONCE A DAY, we need to group by course,timedate & USERID
+        //postgreSQL solve problem on GROUP BY        
+        if ($CFG->dbtype === "pgsql") {
+            $sort = "timedate ASC";
+            $student_resource_access = $DB->get_records_sql("SELECT ROW_NUMBER() over(), a.* FROM (SELECT course, userid, timedate, sum(numval) as numval FROM {block_gismo_sl} WHERE $ctu_filters ORDER BY $sort) as a", $ctu_params);
+        } else {
             $sort = "time ASC";
             $fields = " id, course, userid, timedate, time, sum(numval) as numval"; //BUG FIX WHEN GISMO EXPORTER RUN MORE THEN ONCE A DAY        
             // chart data
